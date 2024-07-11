1a6db1d8fef056210f8403e737ba7545a80f2d74
Porter Young <porteryoung2005@gmail.com>
2024-07-11 15:57:11 -0600

    Able to create note

--- src/db/DbContext.js
+++ src/db/DbContext.js
@@ -2,11 +2,13 @@ import mongoose from 'mongoose'
 import { AccountSchema } from '../models/Account'
 import { ValueSchema } from '../models/Value'
 import { BugsSchema } from '../models/Bug.js';
+import { NoteSchema } from '../models/Note.js';
 
 class DbContext {
   Values = mongoose.model('Value', ValueSchema);
   Account = mongoose.model('Account', AccountSchema);
   Bugs = mongoose.model('Bug', BugsSchema);
+  Notes = mongoose.model('Note', NoteSchema);
 }
 
 export const dbContext = new DbContext()
--- src/models/Note.js
+++ src/models/Note.js
@@ -2,9 +2,23 @@ import { Schema } from "mongoose";
 
 export const NoteSchema = new Schema({
     body: { type: String, minLength: 5, maxLength: 500, required: true},
-    bugId: {type: Schema.ObjectId, required: true},
-    creatorId: {type: Schema.ObjectId, required: true}
+    bugId: {type: Schema.ObjectId, required: true, ref: 'Bug'},
+    creatorId: {type: Schema.ObjectId, required: true, ref: 'Account'}
 }, {
     toJSON: {virtuals: true},
     timestamps: true
+})
+
+NoteSchema.virtual('creator', {
+    localField: 'creatorId',
+    ref: 'Account',
+    foreignField: '_id',
+    justOne: true
+})
+
+NoteSchema.virtual('bug', {
+    localField: 'bugId',
+    ref: 'Bug',
+    foreignField: '_id',
+    justOne: true
 })
